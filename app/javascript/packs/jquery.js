c36b56010673812824d59e35e9dea7c9f1f51429
EC2 Default User <ec2-user@ip-172-31-10-103.ap-northeast-1.compute.internal>
2023-02-08 01:54:04 +0000

    #7終了です

--- app/javascript/packs/jquery.js
+++ app/javascript/packs/jquery.js
@@ -1,18 +1,29 @@
 (()=>{
 
- $(function (){
-  $('.box1').slideDown(function (){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp();
+  $(function() {
+      $('.box1').mouseover(function(){
+        $('.box1').addClass('box1-ext');
+      });
+      $('.box1').mouseout(function(){
+          $('.box1').removeClass('box1-ext');
+      });
   });
-});
 
-    $(function (){
+  $(function() {
+      $('.box2').on('click',function(){
+        $('.box2').addClass('box2-ext');
+      });
+      $('.box2').mouseout(function(){
+          $('.box2').removeClass('box2-ext');
+      });
+  });
+
+  $(function (){
     $('.jquery').on('click', function(){
     $(this).css('color','red');
+  });
+    $('.jquery').mouseout(function(){
+    $(this).css('color','black');
   });
 });
 
